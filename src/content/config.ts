d82a086a9bbc0cf8e9b99e8c4ace5d0338150045
Luke S. Schierer <luke@schierer.org>
2024-09-04 20:27:43 -0400

    feat: collections for types
    SkillBook Collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,27 @@
 import { defineCollection, } from 'astro:content';
 //import { z } from 'astro:schema';
 import { docsSchema } from '@astrojs/starlight/schema';
-import { generalObject } from '../schemas';
+import { General, SkillBook } from '../schemas';
 
-/*
-const generalEntry = z.object({
-  'id': z.string(),
-  'data': generalObject,
-});
-
-type generalEntryType = z.infer<typeof generalEntry>;
-*/
-
-
-const generals = defineCollection({
+const skillBooks = defineCollection({
 	loader: async () => {
-    const response = await fetch("http://localhost:5000/generals/details");
+    const response = await fetch("http://localhost:5000/skillbook/list/details");
     const data = await response.json();
     // Must return an array of entries with an id property,
     // or an object with IDs as keys and entries as values
-    return data.map((general: generalObject) => ({
+    return data.map((book: SkillBook) => ({
+      ...book,
+    }));
+  },
+});
+
+const generals = defineCollection({
+	loader: async () => {
+    const response = await fetch("http://localhost:5000/generals/details?verbose=true&level=1&stars=5&ascendingLevel=5Red&speciality45=Gold&speciality2=Gold&speciality3=Gold&speciality4=Gold");
+    const data = await response.json();
+    // Must return an array of entries with an id property,
+    // or an object with IDs as keys and entries as values
+    return data.map((general: General) => ({
       ...general,
     }));
   },
@@ -27,5 +29,6 @@ const generals = defineCollection({
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
-  generals
+  generals, 
+  skillBooks
 };
