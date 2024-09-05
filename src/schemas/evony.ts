e5afa7ef8c17683c98ca5401c73fa0ee949ff1a0
Luke S. Schierer <luke@schierer.org>
2024-09-04 20:24:54 -0400

    fix: corrected schema files

--- src/schemas/evony.ts
+++ src/schemas/evony.ts
@@ -1,56 +1,120 @@
-import {z} from 'astro:schema';
+import { z } from 'astro:schema';
 
-export const buffObject = z.object({
-  'attribute':  z.string(),
-  'class':      z.tuple([z.string(),z.array(z.string())]).optional(),
-  'condition':  z.array(z.string()).optional(),
-  'inherited':  z.boolean().optional(),
-  'value':      z.object({
-    'number':   z.number(),
-    'unit':     z.string(),
-  }),
+export const Value = z.object({
+  "number": z.number(),
+  "unit": z.string(),
 });
+export type Value = z.infer<typeof Value>;
 
-export const specialityObject = z.object({
-  'activeLevel':  z.string(),
-  'name':         z.string(),
+export const Aes = z.object({
+  "Attack": z.number().optional(),
+  "Overall": z.number(),
+  "Toughness": z.number().optional(),
+  "Preservation": z.number().optional(),
+  "Unused": z.number().optional(),
 });
+export type Aes = z.infer<typeof Aes>;
 
-export const skillBookObject = z.object({
-  'name':   z.string(),
-  'buffs':  z.array(buffObject),
-})
-
-export const generalObject = z.object({
-  'EvAnsScores': z.object({
-    'AttackingAsPrimary': z.number(),
-    'AttackingAsSecondary': z.number(),
-  }),
-  'ascendingAttributes': z.object({
-    '5Red': z.array(buffObject).optional(),
-    '4Red': z.array(buffObject).optional(),
-    '3Red': z.array(buffObject).optional(),
-    '2Red': z.array(buffObject).optional(),
-    '1Red': z.array(buffObject).optional(),
-    '5Purple': z.array(buffObject).optional(),
-    '4Purple': z.array(buffObject).optional(),
-    '3Purple': z.array(buffObject).optional(),
-    '2Purple': z.array(buffObject).optional(),
-    '1Purple': z.array(buffObject).optional(),
-  }).optional(),
-  'attack': z.number(),
-  'defense': z.number(),
-  'politics': z.number(),
-  'leadership': z.number(),
-  'attack_increment': z.number(),
-  'defense_increment': z.number(),
-  'politics_increment': z.number(),
-  'leadership_increment': z.number(),
-  'level': z.number(),
-  'id': z.string(),
-  'specialities': z.array(specialityObject),
-  'builtInBook': skillBookObject,
-  'otherBooks': z.array(skillBookObject).nullish(),
+export const Attack = z.object({
+  "AES": z.number().optional(),
+  "BAS": z.number(),
+  "Overall": z.number(),
+  "SBS": z.number().optional(),
 });
+export type Attack = z.infer<typeof Attack>;
 
-export type generalObject = z.infer<typeof generalObject>;
+export const Bas = z.object({
+  "Attack": z.number(),
+  "Defense": z.number(),
+  "Leadershp": z.number(),
+  "Overall": z.number(),
+  "Politics": z.number(),
+});
+export type Bas = z.infer<typeof Bas>;
+
+export const Cvs = z.object({
+});
+export type Cvs = z.infer<typeof Cvs>;
+
+export const Unused = z.object({
+  "SBS": z.number().optional(),
+  "AES": z.number().optional(),
+});
+export type Unused = z.infer<typeof Unused>;
+
+export const EvAnsScores = z.object({
+  "AttackingAsPrimary": z.number(),
+  "AttackingAsSecondary": z.number(),
+  "MonsterAsPrimary": z.number(),
+  "MonsterAsSecondary": z.number(),
+});
+export type EvAnsScores = z.infer<typeof EvAnsScores>;
+
+export const Buff = z.object({
+  "attribute": z.string(),
+  "class": z.union([z.string(),z.array(z.string())]).optional(),
+  "condition": z.array(z.string()).optional(),
+  "inherited": z.number(),
+  "value": Value,
+});
+export type Buff = z.infer<typeof Buff>;
+
+export const Attacking = z.object({
+  "AES": Aes,
+  "Attack": Attack,
+  "BAS": Bas,
+  "CVS": Cvs,
+  "Preservation": Attack,
+  "SBS": Aes,
+  "SPS": Cvs,
+  "Toughness": Attack,
+  "Unused": Unused.optional(),
+});
+export type Attacking = z.infer<typeof Attacking>;
+
+export const Speciality = z.object({
+  "Blue": z.array(Buff).optional(),
+  "Gold": z.array(Buff).optional(),
+  "Green": z.array(Buff).optional(),
+  "Orange": z.array(Buff).optional(),
+  "Purple": z.array(Buff).optional(),
+  "activeLevel": z.number(),
+  "name": z.string(),
+});
+export type Speciality = z.infer<typeof Speciality>;
+
+export const ComponentScores = z.object({
+  "Attacking": Attacking,
+  "Monster": Attacking,
+});
+export type ComponentScores = z.infer<typeof ComponentScores>;
+
+export const SkillBook = z.object({
+  "id":   z.string().optional(),
+  "buffs": z.array(Buff).optional(),
+  "name": z.string(),
+  "text": z.union([z.null(), z.string()]),
+});
+export type SkillBook = z.infer<typeof SkillBook>;
+
+export const General = z.object({
+  "id": z.string(),
+  "ComponentScores": ComponentScores,
+  "EvAnsScores": EvAnsScores,
+  "ascendingAttributes": z.record(z.string(), z.array(z.string())),
+  "attack": z.number(),
+  "attack_increment": z.number(),
+  "builtInBook": SkillBook,
+  "defense": z.number(),
+  "defense_increment": z.union([z.number(), z.string()]),
+  "hasCovenant": z.boolean(),
+  "name": z.string(),
+  "leadership": z.number(),
+  "leadership_increment": z.union([z.number(), z.string()]),
+  "level": z.number(),
+  "otherBooks": z.array(SkillBook),
+  "politics": z.number(),
+  "politics_increment": z.union([z.number(), z.string()]),
+  "specialities": z.array(Speciality),
+});
+export type General = z.infer<typeof General>;
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,14 @@
 import * as e from './evony';
 
-export const buffObject = e.buffObject;
-export const specialityObject = e.specialityObject;
-export const skillBookObject = e.skillBookObject;
-export const generalObject = e.generalObject;
-export type generalObject = e.generalObject;
+export const buffObject = e.Buff;
 
+export const specialityObject = e.Speciality;
+
+export const SkillBook = e.SkillBook;
+export type SkillBook = e.SkillBook;
+
+export const General = e.General;
+export type General = e.General;
+
+export const Value = e.Value;
+export type Value = e.Value;
