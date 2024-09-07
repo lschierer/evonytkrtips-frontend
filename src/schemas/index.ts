6023ec195435378f09ad18899ce5a485da96e540
Luke S. Schierer <luke@schierer.org>
2024-09-06 22:01:36 -0400

    build: additional exports

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,11 +4,15 @@ export const buffObject = e.Buff;
 
 export const specialityObject = e.Speciality;
 
-export const skillBook = e.skillBook;
-export type skillBook = e.skillBook;
-
+export const Buff = e.Buff;
 export const General = e.General;
-export type General = e.General;
-
+export const skillBook = e.skillBook;
+export const Speciality = e.Speciality;
 export const Value = e.Value;
+
+export type Buff = e.Buff;
+export type General = e.General;
+export type skillBook = e.skillBook;
+export type Speciality = e.Speciality;
 export type Value = e.Value;
+
