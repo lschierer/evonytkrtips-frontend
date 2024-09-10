3e0c92d747dee2067a76a3effb84247eb36fd097
Luke S. Schierer <luke@schierer.org>
2024-09-09 22:45:25 -0400

    feat: perl backend
    correct perl URL

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ import { General, skillBook } from '../schemas';
 
 export const skillBooks = defineCollection({
 	loader: async () => {
-    const response = await fetch("http://localhost:5000/skillbook/list/details?verbose");
+    const response = await fetch("http://localhost:8080/skillbook/list/details?verbose");
     const data = await response.json();
     // Must return an array of entries with an id property,
     // or an object with IDs as keys and entries as values
@@ -17,7 +17,7 @@ export const skillBooks = defineCollection({
 
 export const generals = defineCollection({
 	loader: async () => {
-    const response = await fetch("http://localhost:5000/general/list/details?verbose=true&level=1&stars=5&ascendingLevel=5Red&speciality45=Gold&speciality2=Gold&speciality3=Gold&speciality4=Gold");
+    const response = await fetch("http://localhost:8080/general/list/details?verbose=true&level=1&stars=5&ascendingLevel=5Red&speciality45=Gold&speciality2=Gold&speciality3=Gold&speciality4=Gold");
     const data = await response.json();
     // Must return an array of entries with an id property,
     // or an object with IDs as keys and entries as values
